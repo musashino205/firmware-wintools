2fd1ecfcc8967ddb9ff35c3068637ccd170fb0c5
INAGAKI Hiroshi <musashino.open@gmail.com>
2019-07-09 19:10:13 +0900

    プログラムタイトルを "Firmware-WinTools" にリネーム

--- firmware-wintools/Properties/AssemblyInfo.cs
+++ firmware-wintools/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("firmware-wintools")]
+[assembly: AssemblyTitle("Firmware-WinTools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("firmware-wintools")]
+[assembly: AssemblyProduct("Firmware-WinTools")]
 [assembly: AssemblyCopyright("Copyright ©musashino205  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
