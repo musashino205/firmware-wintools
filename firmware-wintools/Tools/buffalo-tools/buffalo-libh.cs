8ad0c9b956cd0c4d4cc12c86921d22f4a9a68a8f
INAGAKI Hiroshi <musashino.open@gmail.com>
2019-07-16 00:00:11 +0900

    buffalo-enc: 未使用のusingを削除

--- firmware-wintools/Tools/buffalo-tools/buffalo-libh.cs
+++ firmware-wintools/Tools/buffalo-tools/buffalo-libh.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace firmware_wintools.Tools
+﻿namespace firmware_wintools.Tools
 {
 	partial class Buffalo_Lib
 	{
