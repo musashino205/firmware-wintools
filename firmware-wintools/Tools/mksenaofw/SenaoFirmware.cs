d9773b397e618637d370a966700a82c37481cd1f
INAGAKI Hiroshi <musashino.open@gmail.com>
2021-07-11 13:51:56 +0900

    mksenaofw: Firmwareクラスを使用, ファイル入出力周り整理
    - 入出力ファイル取り扱い全体にtry-catch設定
    - 入出力ファイルのオープンにusingを使用

---
+++ firmware-wintools/Tools/mksenaofw/SenaoFirmware.cs
@@ -0,0 +1,215 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace firmware_wintools.Tools
+{
+	class SenaoHeader : HeaderFooter
+	{
+		internal uint head = DEF_HEAD_VALUE;
+		internal uint vendor_id;
+		internal uint product_id;
+		internal byte[] version = new byte[VER_LEN];
+		internal uint firmware_type;
+		internal uint filesize;
+		internal uint zero;
+		internal byte[] md5sum = new byte[MD5_LEN];
+		internal byte[] pad = new byte[PAD_LEN];
+		internal uint cksum;
+		internal uint magic = DEF_MAGIC;
+
+		[NonSerialized]
+		internal const int HDR_LEN = 0x60;
+
+		[NonSerialized]
+		internal const int VER_LEN = 0x10;
+
+		[NonSerialized]
+		internal const int MD5_LEN = 0x10;
+
+		[NonSerialized]
+		internal const int PAD_LEN = 0x20;
+
+		[NonSerialized]
+		internal const uint DEF_HEAD_VALUE = 0x0;
+
+		[NonSerialized]
+		internal const string DEF_VERSION = "123";
+
+		[NonSerialized]
+		internal const uint DEF_MAGIC = 0x12345678;
+
+		/// <summary>
+		/// ヘッダ部分のチェックサムを算出します。
+		/// </summary>
+		/// <param name="len">算出対象のデータ長</param>
+		/// <returns>チェックサム値</returns>
+		internal uint CalcHeaderCksum(long len)
+		{
+			uint sum = 0;
+			byte[] buf = new byte[len];
+
+			SerializeProps(ref buf, 0);
+
+			for (long i = 0; i < len; ++i)
+				sum += buf[i];
+
+			return sum;
+		}
+
+		internal int LoadHeader(in FileStream fs)
+		{
+			byte[] buf = new byte[sizeof(uint)];
+
+			/* head */
+			if (fs.Read(buf, 0, sizeof(uint)) != sizeof(uint))
+				return 1;
+			head = BitConverter.ToUInt32(buf, 0);
+
+			/* vendor_id */
+			if (fs.Read(buf, 0, sizeof(uint)) != sizeof(uint))
+				return 1;
+			vendor_id = (uint)IPAddress.NetworkToHostOrder(
+						BitConverter.ToInt32(buf, 0));
+
+			/* product_id */
+			if (fs.Read(buf, 0, sizeof(uint)) != sizeof(uint))
+				return 1;
+			product_id = (uint)IPAddress.NetworkToHostOrder(
+						BitConverter.ToInt32(buf, 0));
+
+			/* version */
+			if (fs.Read(version, 0, VER_LEN) != VER_LEN)
+				return 1;
+
+			/* firmware_type */
+			if (fs.Read(buf, 0, sizeof(uint)) != sizeof(uint))
+				return 1;
+			firmware_type = (uint)IPAddress.NetworkToHostOrder(
+						BitConverter.ToInt32(buf, 0));
+
+			/* filesize */
+			if (fs.Read(buf, 0, sizeof(uint)) != sizeof(uint))
+				return 1;
+			filesize = (uint)IPAddress.NetworkToHostOrder(
+						BitConverter.ToInt32(buf, 0));
+
+			/* zero */
+			if (fs.Length - fs.Position < sizeof(uint))
+				return 1;
+			fs.Seek(sizeof(uint), SeekOrigin.Current);
+
+			/* md5sum */
+			if (fs.Read(md5sum, 0, MD5_LEN) != MD5_LEN)
+				return 1;
+
+			/* pad */
+			if (fs.Length - fs.Position < PAD_LEN)
+				return 1;
+			fs.Seek(PAD_LEN, SeekOrigin.Current);
+
+			/* cksum */
+			if (fs.Read(buf, 0, sizeof(uint)) != sizeof(uint))
+				return 1;
+			cksum = (uint)IPAddress.NetworkToHostOrder(
+						BitConverter.ToInt32(buf, 0));
+
+			/* magic */
+			if (fs.Read(buf, 0, sizeof(uint)) != sizeof(uint))
+				return 1;
+			magic = (uint)IPAddress.NetworkToHostOrder(
+						BitConverter.ToInt32(buf, 0));
+
+			return 0;
+		}
+
+		/// <summary>
+		/// <code>firmware_type</code> の値から有効なファームウェアタイプであるかをチェックします。
+		/// </summary>
+		/// <returns>有効: true, 無効: false</returns>
+		internal static bool ChkFwType(uint fw_type)
+		{
+			byte type = BitConverter.GetBytes(fw_type)[0];
+
+			if (!FirmwareType.TYPES.ContainsKey(type))
+			{
+				Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix +
+					Lang.Tools.MkSenaoFwRes.Error_NoInvalidFwType);
+
+				return false;
+			}
+
+			/* type = 0 ("combo") */
+			if (fw_type == 0)
+			{
+				Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix +
+					Lang.Tools.MkSenaoFwRes.Error_NoImplCombo);
+				return false;
+			}
+
+			return true;
+		}
+
+		internal class FirmwareType
+		{
+			internal const byte TYPE_NONE = 0xFF;
+
+			internal static readonly
+			Dictionary<byte, TypeInfo> TYPES = new Dictionary<byte, TypeInfo>()
+			{
+			{ 0x00, new TypeInfo(){ name = "combo", comment = "(not implemented)" } },	// 実装省略
+			{ 0x01, new TypeInfo(){ name = "bootloader" } },
+			{ 0x02, new TypeInfo(){ name = "kernel" } },
+			{ 0x03, new TypeInfo(){ name = "kernelapp" } },
+			{ 0x04, new TypeInfo(){ name = "apps" } },
+			/* 以下メーカー依存の値 */
+			{ 0x05, new TypeInfo(){ name = "littleapps", comment = "(D-Link)/factoryapps (EnGenius)" } },
+			{ 0x06, new TypeInfo(){ name = "sounds", comment = "(D-Link)/littleapps (EnGenius)" } },
+			{ 0x07, new TypeInfo(){ name = "userconfig", comment = "(D-Link)/appdata (EnGenius)" } },
+			{ 0x08, new TypeInfo(){ name = "userconfig", comment = "(EnGenius)" } },
+			{ 0x09, new TypeInfo(){ name = "odmapps", comment = "(EnGenius)" } },
+			{ 0x0a, new TypeInfo(){ name = "factoryapps", comment = "(D-Link)" } },
+			{ 0x0b, new TypeInfo(){ name = "odmapps", comment = "(D-Link)" } },
+			{ 0x0c, new TypeInfo(){ name = "langpack", comment =  "(D-Link)" } }
+			};
+
+			internal class TypeInfo
+			{
+				internal string name;
+				internal string comment = "";
+			}
+		}
+	}
+
+	class SenaoFirmware : Firmware
+	{
+		internal const int DEF_BLOCK_SIZE = 65535;
+
+		/* common */
+		internal void EncodeData(uint magic)
+		{
+			for (long i = 0; i < data.Length; i++)
+				data[i] ^= (byte)(magic >> (int)(i % 8) & 0xff);
+		}
+
+		/* for encoding */
+		internal byte[] GetMd5sum()
+		{
+			MD5CryptoServiceProvider md5p =
+				new MD5CryptoServiceProvider();
+
+			return md5p.ComputeHash(inFs);
+		}
+
+		/* for decoding */
+		internal int LoadData(long length)
+		{
+			data = new byte[length];
+
+			return (FileToBytes(in inFs, ref data, length) == length) ?
+				0 : 1;
+		}
+	}
+}
--- firmware-wintools/Tools/mksenaofw/mksenaofw.cs
+++ firmware-wintools/Tools/mksenaofw/mksenaofw.cs
@@ -1,25 +1,13 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace firmware_wintools.Tools
 {
 	class MkSenaoFw
 	{
-		const int HDR_LEN = 0x60;
-		const int BUF_SIZE = 0x200;
-		const int VERSION_SIZE = 0x10;
-		const int MD5_SIZE = 0x10;
-		const int PAD_SIZE = 0x20;
-
-		const int DEFAULT_BLOCK_SIZE = 65535;
-
-		const int DEFAULT_HEAD_VALUE = 0x0;
-		const string DEFAULT_VERSION = "123";
-		const int DEFAULT_MAGIC = 0x12345678;
-
 		byte[] md5sum;
 
 		/// <summary>
@@ -61,50 +49,6 @@ namespace firmware_wintools.Tools
 			internal int bs;
 		}
 
-		/// <summary>
-		/// Senaoヘッダ構造体
-		/// </summary>
-		public struct Header
-		{
-			internal uint head;
-			internal uint vendor_id;
-			internal uint product_id;
-			internal byte[] version;
-			internal uint fw_type;
-			internal int filesize;
-			internal uint zero;
-			internal byte[] md5sum;
-			internal byte[] pad;
-			internal uint cksum;
-			internal uint magic;
-		}
-
-		/// <summary>
-		/// ファームウェアタイプ配列
-		/// </summary>
-		static readonly Firmware_Type[] FIRMWARE_TYPES = new Firmware_Type[]
-		{
-			new Firmware_Type() { id = 0x00, name = "combo", comment = "(not implemented)" },	// 実装省略
-			new Firmware_Type() { id = 0x01, name = "bootloader" },
-			new Firmware_Type() { id = 0x02, name = "kernel" },
-			new Firmware_Type() { id = 0x03, name = "kernelapp" },
-			new Firmware_Type() { id = 0x04, name = "apps" },
-			/* 以下メーカー依存の値 */
-			new Firmware_Type() { id = 0x05, name = "littleapps (D-Link)/factoryapps (EnGenius)" },
-			new Firmware_Type() { id = 0x06, name = "sounds (D-Link)/littleapps (EnGeinus)" },
-			new Firmware_Type() { id = 0x07, name = "userconfig (D-Link)/appdata (EnGeinus)" },
-			new Firmware_Type() { id = 0x08, name = "userconfig (EnGeinus)" },
-			new Firmware_Type() { id = 0x09, name = "odmapps (EnGeinus)" },
-			new Firmware_Type() { id = 0x0a, name = "factoryapps (D-Link)" },
-			new Firmware_Type() { id = 0x0b, name = "odmapps (D-Link)" },
-			new Firmware_Type() { id = 0x0c, name = "langpack (D-Link)" }
-		};
-
-		/// <summary>
-		/// ファームウェアタイプ未指定値
-		/// </summary>
-		const byte FIRMEARE_TYPE_NONE = 0xFF;
-
 		private void PrintHelp(int arg_idx)
 		{
 			Console.WriteLine(Lang.Tools.MkSenaoFwRes.Help_Usage +
@@ -117,13 +61,13 @@ namespace firmware_wintools.Tools
 			Console.WriteLine(Lang.CommonRes.Help_FunctionOpts +
 				Lang.Tools.MkSenaoFwRes.Help_Options_t +
 				Lang.Tools.MkSenaoFwRes.Help_Options_t_values);
-			for (int i = 0; i < FIRMWARE_TYPES.Length; i++)     // firmware types
+			foreach (KeyValuePair<byte, SenaoHeader.FirmwareType.TypeInfo> pair
+					in SenaoHeader.FirmwareType.TYPES)
 			{
 				Console.WriteLine(Lang.Tools.MkSenaoFwRes.Help_Options_t_TypeFmt,
-					FIRMWARE_TYPES[i].id < 10 ?
-						FIRMWARE_TYPES[i].id.ToString() + " " : FIRMWARE_TYPES[i].id.ToString(),
-					FIRMWARE_TYPES[i].name,
-					FIRMWARE_TYPES[i].comment);
+					pair.Key,
+					pair.Value.name,
+					pair.Value.comment);
 			}
 			Console.WriteLine(Lang.Tools.MkSenaoFwRes.Help_Options_t_Line +
 				Lang.Tools.MkSenaoFwRes.Help_Options_v +
@@ -138,9 +82,12 @@ namespace firmware_wintools.Tools
 		private void PrintInfo(Properties subprops)
 		{
 			Console.WriteLine(Lang.Tools.MkSenaoFwRes.Info,
-				subprops.isde ? Lang.Tools.MkSenaoFwRes.Info_Decode: Lang.Tools.MkSenaoFwRes.Info_Encode);
+				subprops.isde ?
+					Lang.Tools.MkSenaoFwRes.Info_Decode :
+					Lang.Tools.MkSenaoFwRes.Info_Encode);
 			Console.WriteLine(Lang.Tools.MkSenaoFwRes.Info_FwType,
-				subprops.fw_type, FIRMWARE_TYPES[subprops.fw_type].name);
+				subprops.fw_type,
+				SenaoHeader.FirmwareType.TYPES[subprops.fw_type].name);
 			Console.WriteLine(Lang.Tools.MkSenaoFwRes.Info_FwVer, subprops.version);
 			Console.WriteLine(Lang.Tools.MkSenaoFwRes.Info_Vendor, subprops.vendor);
 			Console.WriteLine(Lang.Tools.MkSenaoFwRes.Info_Product, subprops.product);
@@ -159,209 +106,127 @@ namespace firmware_wintools.Tools
 		}
 
 		/// <summary>
-		/// <paramref name="fw_type"/> の値から有効なファームウェアタイプであるかをチェックします。
-		/// </summary>
-		/// <param name="fw_type"></param>
-		/// <returns>有効: 0, 無効: 1</returns>
-		private int ChkFwType(uint fw_type)
-		{
-			if (fw_type > FIRMWARE_TYPES.Length - 1 ||	// 渡されたtypeが配列長を超過
-				fw_type == FIRMEARE_TYPE_NONE)			// または未指定状態
-			{
-				Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix + 
-					Lang.Tools.MkSenaoFwRes.Error_NoInvalidFwType);
-				return 1;
-			}
-			else if (fw_type == 0)						// typeが "combo"
-			{
-				Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix +
-					Lang.Tools.MkSenaoFwRes.Error_NoImplCombo);
-				return 1;
-			}
-
-			return 0;
-		}
-
-		/// <summary>
-		/// ヘッダ部分のチェックサムを算出します。
-		/// </summary>
-		/// <param name="data">算出対象データ</param>
-		/// <param name="len">算出対象のデータ長</param>
-		/// <returns>算出成功: チェックサム値, 失敗: -1</returns>
-		private int Calc_HeaderCksum(in byte[] data, int len)
-		{
-			int sum = 0;
-
-			if (data == null || len <= 0)
-				return -1;
-
-			for (int i = 0; i < len; ++i)			
-				sum += data[i];
-
-			return sum;
-		}
-
-		/// <summary>
 		/// 渡されたパラメータを基にしてデータのエンコードとヘッダの付加を行います。
 		/// </summary>
-		/// <param name="subprops">mksenaofw機能プロパティ</param>
-		/// <param name="inFs">入力ファイルのFileStream</param>
-		/// <param name="outFs">出力ファイルのFileStream</param>
-		/// <returns>成功: 0, 失敗: 1</returns>
-		private int Encode(Properties subprops, ref FileStream inFs, ref FileStream outFs, bool quiet)
+		/// <param name="fw">SenaoFirmwareクラス</param>
+		/// <param name="props">メインプロパティ</param>
+		/// <param name="subprops">機能プロパティ</param>
+		/// <returns></returns>
+		private int Encode(ref SenaoFirmware fw, Program.Properties props, Properties subprops)
 		{
-			int filesize, cksum, read_len, pad_len, avail_len;
-			byte[] buf = new byte[BUF_SIZE];
-			MD5CryptoServiceProvider md5p = new MD5CryptoServiceProvider();
-			Header fw_header = new Header()
+			long pad_len;
+
+			SenaoHeader header = new SenaoHeader()
 			{
-				head = DEFAULT_HEAD_VALUE,
 				vendor_id = (uint)IPAddress.HostToNetworkOrder((int)subprops.vendor),
 				product_id = (uint)IPAddress.HostToNetworkOrder((int)subprops.product),
-				version = Encoding.ASCII.GetBytes(subprops.version),
-				fw_type = (uint)IPAddress.HostToNetworkOrder((int)subprops.fw_type),
-				//filesize
-				zero = 0,
-				md5sum = md5p.ComputeHash(inFs),
-				pad = new byte[PAD_SIZE],
-				//cksum
-				magic = (uint)IPAddress.HostToNetworkOrder((int)subprops.magic)
+				firmware_type = (uint)IPAddress.HostToNetworkOrder((int)subprops.fw_type),
+				filesize = (uint)IPAddress.HostToNetworkOrder((int)fw.inFInfo.Length),
+				magic = (uint)IPAddress.HostToNetworkOrder((int)subprops.magic),
+				totalLen = SenaoHeader.HDR_LEN
 			};
 
-			/* MD5CryptoServiceProvider.ComputeHash(Stream)でPositionが末尾まで飛ぶ */
-			inFs.Position = 0;
-
-			if (inFs.Length > 0x7FFFFFFFL)
-			{
-				Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix +
-					Lang.Tools.MkSenaoFwRes.Error_LargeInFile);
-				return 1;
-			}
-			filesize = Convert.ToInt32(inFs.Length);
-
-			md5sum = fw_header.md5sum;
-
-			if (!quiet)
-				PrintInfo(subprops);
+			/* versionをコピー */
+			Array.Copy(
+				Encoding.ASCII.GetBytes(subprops.version),
+				header.version,
+				Encoding.ASCII.GetByteCount(subprops.version) > SenaoHeader.VER_LEN ?
+					SenaoHeader.VER_LEN :
+					Encoding.ASCII.GetByteCount(subprops.version));
 
 			/* パディングサイズ */
-			pad_len = subprops.pad && subprops.bs > 0 ? 
-				subprops.bs - (filesize % subprops.bs) : 0;
+			pad_len = subprops.bs > 0 ?
+					(subprops.bs - (fw.inFInfo.Length % subprops.bs)) : 0;
 
-			fw_header.filesize = IPAddress.HostToNetworkOrder(filesize);
-
-			Array.Copy(BitConverter.GetBytes(fw_header.vendor_id), 0,
-				buf, sizeof(uint), sizeof(uint));
-			Array.Copy(BitConverter.GetBytes(fw_header.product_id), 0, 
-				buf, sizeof(uint) * 2, sizeof(uint));
-			Array.Copy(fw_header.version, 0, buf, sizeof(uint) * 3, fw_header.version.Length);
-			Array.Copy(BitConverter.GetBytes(fw_header.fw_type), 0,
-				buf, sizeof(uint) * 7, sizeof(uint));
-			Array.Copy(BitConverter.GetBytes(fw_header.filesize), 0,
-				buf, sizeof(uint) * 8, sizeof(uint));
-			Array.Copy(fw_header.md5sum, 0, buf, sizeof(uint) * 10, MD5_SIZE);
-			/* Checksumの算出/コピーはMagicをbufにコピーした後 */
-			Array.Copy(BitConverter.GetBytes(fw_header.magic), 0,
-				buf, HDR_LEN - sizeof(uint), sizeof(uint));
-
-			if ((cksum = Calc_HeaderCksum(in buf, HDR_LEN)) < 0)
+			try
 			{
-				Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix +
-					Lang.Tools.MkSenaoFwRes.Error_FailHeaderCksum);
+				using (fw.inFs = new FileStream(props.inFile, FileMode.Open,
+							FileAccess.Read, FileShare.Read))
+				{
+					md5sum = header.md5sum = fw.GetMd5sum();
+					/* MD5sum読み取りでStreamのPositionが末尾まで飛ぶ */
+					fw.inFs.Seek(0, SeekOrigin.Begin);
+
+					/* パディング分もencodeする為fw.dataで扱う */
+					fw.data = new byte[fw.inFInfo.Length + pad_len];
+					Firmware.FileToBytes(in fw.inFs, ref fw.data, fw.inFInfo.Length);
+				}
+			}
+			catch (IOException e)
+			{
+				Console.Error.WriteLine(e.Message);
 				return 1;
 			}
-			fw_header.cksum = (uint)IPAddress.HostToNetworkOrder(cksum);
-			Array.Copy(BitConverter.GetBytes(fw_header.cksum), 0,
-				buf, HDR_LEN - sizeof(uint) * 2, sizeof(uint));
 
-			/* ヘッダ書き込み */
-			outFs.Write(buf, 0, HDR_LEN);
+			header.cksum = (uint)IPAddress.HostToNetworkOrder(
+						(int)header.CalcHeaderCksum(SenaoHeader.HDR_LEN));
 
-			/* データをencodeして書き込み */
-			while ((read_len = inFs.Read(buf, 0, BUF_SIZE)) > 0 || pad_len > 0)
-			{
-				if (read_len < BUF_SIZE && pad_len > 0)	// bufに余りがありパディングが残っている
-				{
-					avail_len = BUF_SIZE - read_len;
-					Array.Clear(buf, read_len, avail_len);
-					read_len += avail_len < pad_len ? avail_len : pad_len;
-					pad_len -= avail_len < pad_len ? avail_len : pad_len;
-				}
+			if (!props.quiet)
+				PrintInfo(subprops);
 
-				for (int i = 0; i < read_len; i++)
-					buf[i] ^= (byte)(subprops.magic >> (i % 8) & 0xff);
-				outFs.Write(buf, 0, read_len);
-			}
+			/* ヘッダシリアル化 */
+			fw.header = new byte[SenaoHeader.HDR_LEN];
+			if (header.SerializeProps(ref fw.header, 0) != SenaoHeader.HDR_LEN)
+				return 1;
 
-			return 0;
+			/* データエンコード */
+			fw.EncodeData(subprops.magic);
+
+			return fw.OpenAndWriteToFile(false);
 		}
 
 		/// <summary>
 		/// 渡されたパラメータを基に、データのデコードを行います。
 		/// </summary>
-		/// <param name="inFs">入力ファイルのFileStream</param>
-		/// <param name="outFs">出力ファイルのFileStream</param>
-		/// <returns>成功: 0, 失敗: 1</returns>
-		private int Decode(ref FileStream inFs, ref FileStream outFs, bool quiet)
+		/// <param name="fw">SenaoFirmwareクラス</param>
+		/// <param name="props">メインプロパティ</param>
+		/// <param name="subprops">機能プロパティ</param>
+		/// <returns></returns>
+		private int Decode(ref SenaoFirmware fw, Program.Properties props, Properties subprops)
 		{
-			int read_len, written_len = 0;
-			byte[] buf = new byte[BUF_SIZE];
-			Properties subprops = new Properties()
+			SenaoHeader header = new SenaoHeader();
+
+			try
 			{
-				isde = true
-			};
-			Header fw_header = new Header()
-			{
-				version = new byte[VERSION_SIZE],
-				md5sum = new byte[MD5_SIZE]
-			};
-
-			inFs.Read(buf, 0, HDR_LEN);
-
-			fw_header.head = BitConverter.ToUInt32(buf, 0);
-			fw_header.vendor_id = (uint)IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buf, sizeof(uint)));
-			fw_header.product_id = (uint)IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buf, sizeof(uint) * 2));
-			Array.Copy(buf, sizeof(uint) * 3, fw_header.version, 0, VERSION_SIZE);
-			fw_header.fw_type =
-				(uint)IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buf, sizeof(uint) * 3 + VERSION_SIZE));
-			fw_header.filesize = 
-				IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buf, sizeof(uint) * 4 + VERSION_SIZE));
-			Array.Copy(buf, HDR_LEN - sizeof(uint) * 2 - PAD_SIZE - MD5_SIZE, fw_header.md5sum, 0, MD5_SIZE);
-			fw_header.cksum =
-				(uint)IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buf, HDR_LEN - sizeof(uint) * 2));
-			fw_header.magic =
-				(uint)IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buf, HDR_LEN - sizeof(uint)));
-
-			subprops.fw_type = BitConverter.GetBytes(fw_header.fw_type)[0];
-			subprops.vendor = fw_header.vendor_id;
-			subprops.product = fw_header.product_id;
-			subprops.version = Encoding.ASCII.GetString(fw_header.version);
-			subprops.magic = fw_header.magic;
-			md5sum = fw_header.md5sum;
-
-			if (!quiet)
-				PrintInfo(subprops);
-			if (ChkFwType(fw_header.fw_type) != 0)
-				return 1;
-
-			while ((read_len = inFs.Read(buf, 0, BUF_SIZE)) > 0)
-			{
-				for (int i = 0; i < read_len; i++)
-					buf[i] ^= (byte)(fw_header.magic >> (i % 8) & 0xff);
-
-				if (written_len + read_len > fw_header.filesize) // 最後パディングされている場合
+				using (fw.inFs = new FileStream(props.inFile, FileMode.Open,
+							FileAccess.Read, FileShare.Read))
 				{
-					read_len = fw_header.filesize - written_len;
-					if (read_len > 0)
-						outFs.Write(buf, 0, read_len);
-					break;
-				}
+					if (header.LoadHeader(in fw.inFs) != 0)
+						return 1;
 
-				outFs.Write(buf, 0, read_len);
-				written_len += read_len;
+					if (fw.LoadData(header.filesize) != 0)
+						return 1;
+				}
+			}
+			catch (IOException e)
+			{
+				Console.Error.WriteLine(e.Message);
+				return 1;
 			}
 
-			return 0;
+			if (header.filesize > int.MaxValue)
+			{
+				Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix +
+					Lang.Tools.MkSenaoFwRes.Error_LargeInFile);
+				return 1;
+			}
+
+			subprops.fw_type = BitConverter.GetBytes(header.firmware_type)[0];
+			subprops.vendor = header.vendor_id;
+			subprops.product = header.product_id;
+			subprops.version = Encoding.ASCII.GetString(header.version);
+			subprops.magic = header.magic;
+			md5sum = header.md5sum;
+
+			if (!props.quiet)
+				PrintInfo(subprops);
+
+			if (!SenaoHeader.ChkFwType(header.firmware_type))
+				return 1;
+
+			fw.EncodeData(header.magic);
+
+			return fw.OpenAndWriteToFile(true);
 		}
 
 		/// <summary>
@@ -374,12 +239,13 @@ namespace firmware_wintools.Tools
 		/// <returns>成功: 0, 失敗: 1</returns>
 		public int Do_MkSenaoFw(string[] args, int arg_idx, Program.Properties props)
 		{
+			SenaoFirmware fw = new SenaoFirmware();
 			Properties subprops = new Properties()
 			{
-				fw_type = FIRMEARE_TYPE_NONE,
-				version = DEFAULT_VERSION,
-				magic = DEFAULT_MAGIC,
-				bs = DEFAULT_BLOCK_SIZE
+				fw_type = SenaoHeader.FirmwareType.TYPE_NONE,
+				version = SenaoHeader.DEF_VERSION,
+				magic = SenaoHeader.DEF_MAGIC,
+				bs = SenaoFirmware.DEF_BLOCK_SIZE
 			};
 
 			if (props.help)
@@ -391,13 +257,20 @@ namespace firmware_wintools.Tools
 			ToolsArgMap argMap = new ToolsArgMap();
 			argMap.Init_args_MkSenaoFw(args, arg_idx, ref subprops);
 
-			if (!subprops.isde)	// エンコード時
+			if (!subprops.pad)
+				subprops.bs = 0;
+
+			fw.inFInfo = new FileInfo(props.inFile);
+
+			/* エンコード時 */
+			if (!subprops.isde)
 			{
-				if (ChkFwType(Convert.ToUInt32(subprops.fw_type)) != 0)
+				if (!SenaoHeader.ChkFwType(Convert.ToUInt32(subprops.fw_type)))
 					return 1;
 
-				if (Encoding.ASCII.GetByteCount(subprops.version) > sizeof(uint) * 4 ||	// 0 < version len < 17
-					Encoding.ASCII.GetByteCount(subprops.version) == 0)
+				/* 0 < version len < 17 */
+				if (Encoding.ASCII.GetByteCount(subprops.version) > SenaoHeader.VER_LEN ||
+				    Encoding.ASCII.GetByteCount(subprops.version) == 0)
 				{
 					Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix +
 						Lang.Tools.MkSenaoFwRes.Error_InvalidVerLen);
@@ -411,31 +284,25 @@ namespace firmware_wintools.Tools
 						Lang.Tools.MkSenaoFwRes.Error_NoInvalidVenProd);
 					return 1;
 				}
+
+				/*
+				 * decode時はfilesizeの長さしか扱わない
+				 * .NETのArrayで扱える要素数はintの範囲内
+				 */
+				if (fw.inFInfo.Length > int.MaxValue)
+				{
+					Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix +
+						Lang.Tools.MkSenaoFwRes.Error_LargeInFile);
+					return 1;
+				}
 			}
 
-			FileStream inFs;
-			FileStream outFs;
-			FileMode outFMode =
-				File.Exists(props.outFile) ? FileMode.Truncate : FileMode.Create;
-			try
-			{
-				inFs = new FileStream(props.inFile, FileMode.Open, FileAccess.Read, FileShare.Read);
-				outFs = new FileStream(props.outFile, outFMode, FileAccess.Write, FileShare.None);
-			}
-			catch (IOException e)
-			{
-				Console.Error.WriteLine(e.Message);
-				return 1;
-			}
+			fw.outFile = props.outFile;
+			fw.outFMode = FileMode.Create;
 
-			int ret = subprops.isde ?
-				Decode(ref inFs, ref outFs, props.quiet) :
-				Encode(subprops, ref inFs, ref outFs, props.quiet);
-
-			inFs.Close();
-			outFs.Close();
-
-			return ret;
+			return subprops.isde ?
+					Decode(ref fw, props, subprops) :
+					Encode(ref fw, props, subprops);
 		}
 	}
 
