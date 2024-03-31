24d38c202144ca236e97c18e9bd8c1384210a65a
INAGAKI Hiroshi <musashino.open@gmail.com>
2024-03-31 17:40:06 +0900

    xorimage: コマンドラインオプションをリスト化

--- firmware-wintools/Tools/xorimage/xorimage.cs
+++ firmware-wintools/Tools/xorimage/xorimage.cs
@@ -1,63 +1,36 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Text;
 
 namespace firmware_wintools.Tools
 {
-	internal partial class XorImage : Tool
+	internal class XorImage : Tool
 	{
 		/* ツール情報　*/
 		public override string name { get => "xorimage"; }
 		public override string desc { get => Lang.Tools.XorImageRes.FuncDesc; }
 		public override string descFmt { get => Lang.Tools.XorImageRes.Main_FuncDesc_Fmt; }
+		public override string resName => "XorImageRes";
 
 
-		/// <summary>
-		/// xorを行うデータ長
-		/// </summary>
-		private string len_s = null;
-		/// <summary>
-		/// xorを開始するデータのオフセット
-		/// </summary>
-		private long offset = 0;
-		/// <summary>
-		/// xorに用いるpattern
-		/// </summary>
-		private string pattern = "12345678";
-		/// <summary>
-		/// 指定されたパターンがhex値であるか否か
-		/// </summary>
-		private bool ishex = false;
-		/// <summary>
-		/// 部分書き換えモード
-		/// </summary>
-		private bool rewrite = false;
-		/// <summary>
-		/// pattern用バイナリファイル
-		/// </summary>
-		private string binPattern = null;
+		private long Length = long.MinValue;
+		private long Offset = 0;
+		private string Pattern = "12345678";
+		private bool IsHex = false;
+		private bool Rewrite = false;
+		private string BinPattern = null;
 
-		/// <summary>
-		/// xorimageの機能ヘルプを表示します
-		/// </summary>
-		public static void PrintHelp(int arg_idx)
+		internal override List<Param> ParamList => new List<Param>()
 		{
-			Console.WriteLine(Lang.Tools.XorImageRes.Help_Usage +
-				Lang.Tools.XorImageRes.FuncDesc +
-				Environment.NewLine,
-				arg_idx < 2 ? "" : "firmware-wintools ");	// 引数インデックスが2未満（symlink呼び出し）の場合機能名のみ
-			// 共通オプション表示
-			Program.PrintCommonOption();
-			// 機能オプション表示
-			Console.WriteLine(Lang.CommonRes.Help_FunctionOpts +
-				Lang.Tools.XorImageRes.Help_Options_Pattern +
-				Lang.Tools.XorImageRes.Help_Options_PatternBin +
-				Lang.Tools.XorImageRes.Help_Options_Hex +
-				Lang.Tools.XorImageRes.Help_Options_Length +
-				Lang.Tools.XorImageRes.Help_Options_Offset +
-				Lang.Tools.XorImageRes.Help_Options_Rewrite);
-		}
+			new Param() { PChar = 'p', PType = Param.PTYPE.STR, SetField = "Pattern", HelpKey = "Help_Options_Patern" },
+			new Param() { PChar = 'P', PType = Param.PTYPE.STR, SetField = "BinPattern", HelpKey = "Help_Options_PatternBin" },
+			new Param() { PChar = 'x', PType = Param.PTYPE.BOOL, SetField = "IsHex", HelpKey = "Help_Options_Hex" },
+			new Param() { PChar = 'l', PType = Param.PTYPE.LONG, SetField = "Length", HelpKey = "Help_Options_Length" },
+			new Param() { PChar = 'O', PType = Param.PTYPE.LONG, SetField = "Offset", HelpKey = "Help_Options_Offset" },
+			new Param() { PChar = 'r', PType = Param.PTYPE.BOOL, SetField = "Rewrite", HelpKey = "Help_Options_Rewrite" }
+		};
 
 		/// <summary>
 		/// xorimageの実行情報を表示します
@@ -67,13 +40,13 @@ namespace firmware_wintools.Tools
 		{
 			Console.WriteLine(Lang.Tools.XorImageRes.Info);
 			Console.WriteLine(Lang.Tools.XorImageRes.Info_Pattern,
-						binPattern != null ?
+						BinPattern != null ?
 							BitConverter.ToString(ptnAry).Replace("-", "") :
-							pattern);
-			Console.WriteLine(Lang.Tools.XorImageRes.Info_Hex, ishex.ToString());
+							Pattern);
+			Console.WriteLine(Lang.Tools.XorImageRes.Info_Hex, IsHex.ToString());
 			Console.WriteLine(Lang.Tools.XorImageRes.Info_len, datalen);
-			Console.WriteLine(Lang.Tools.XorImageRes.Info_offset, offset);
-			Console.WriteLine(Lang.Tools.XorImageRes.Info_Rewrite, rewrite);
+			Console.WriteLine(Lang.Tools.XorImageRes.Info_offset, Offset);
+			Console.WriteLine(Lang.Tools.XorImageRes.Info_Rewrite, Rewrite);
 		}
 
 		/// <summary>
@@ -111,16 +84,16 @@ namespace firmware_wintools.Tools
 			p_len = 0;
 
 			/* パターンバイナリ */
-			if (binPattern != null)
+			if (BinPattern != null)
 			{
 				/*
-				 * - binPatternが空文字
+				 * - BinPatternが空文字
 				 * - ファイルが存在しない
 				 * - ファイルサイズが 0x100 (256 bytes) 超
 				 */
-				if (binPattern.Length == 0 ||
-				    !File.Exists(binPattern) ||
-				    (ptnBinInfo = new FileInfo(binPattern)).Length > hexPtn_maxLen)
+				if (BinPattern.Length == 0 ||
+				    !File.Exists(BinPattern) ||
+				    (ptnBinInfo = new FileInfo(BinPattern)).Length > hexPtn_maxLen)
 				{
 					Console.Error.WriteLine(Lang.Resource.Main_Error_Prefix +
 								Lang.Tools.XorImageRes.Error_InvalidPatternBin);
@@ -133,7 +106,7 @@ namespace firmware_wintools.Tools
 				ptnAry = new byte[ptnBinInfo.Length];
 				try
 				{
-					using (FileStream ptnFs = new FileStream(binPattern, FileMode.Open,
+					using (FileStream ptnFs = new FileStream(BinPattern, FileMode.Open,
 												FileAccess.Read, FileShare.Read))
 					{
 						ptnFs.Read(ptnAry, 0, (int)ptnFs.Length);
@@ -146,13 +119,13 @@ namespace firmware_wintools.Tools
 					return 1;
 				}
 
-				ishex = true;
+				IsHex = true;
 				p_len = ptnAry.Length;
 				return 0;
 			}
 
 			/* テキストパターン */
-			p_len = pattern.Length;
+			p_len = Pattern.Length;
 			if (p_len == 0)
 			{
 				Console.Error.WriteLine(
@@ -160,7 +133,7 @@ namespace firmware_wintools.Tools
 				return 1;
 			}
 
-			if (ishex)
+			if (IsHex)
 			{
 				if ((p_len / 2) > hexPtn_maxLen)
 				{
@@ -179,10 +152,10 @@ namespace firmware_wintools.Tools
 				p_len /= 2;
 				ptnAry = new byte[p_len];
 				for (int i = 0; i < p_len; i++)
-					ptnAry[i] = Convert.ToByte(pattern.Substring(i * 2, 2), 16);
+					ptnAry[i] = Convert.ToByte(Pattern.Substring(i * 2, 2), 16);
 			}
 			else
-				ptnAry = Encoding.ASCII.GetBytes(pattern);
+				ptnAry = Encoding.ASCII.GetBytes(Pattern);
 
 			return 0;
 		}
@@ -198,12 +171,12 @@ namespace firmware_wintools.Tools
 		internal override int Do(string[] args, int arg_idx, Program.Properties props)
 		{
 			int read_len, write_len, p_len, p_off = 0;
-			long len = long.MaxValue;
 			byte[] ptnAry;
 			Firmware fw = new Firmware()
 			{
 				data = new byte[4096]
 			};
+			int ret;
 
 			if (props.help)
 			{
@@ -211,42 +184,42 @@ namespace firmware_wintools.Tools
 				return 0;
 			}
 
-			Init_args(args, arg_idx);
+			ret = InitArgs(args, arg_idx);
+			if (ret != 0)
+				return ret;
 
-			if (SetupPattern(out ptnAry, out p_len) > 0)
+			if (SetupPattern(out ptnAry, out p_len) != 0)
 				return 1;
 
 			fw.inFInfo = new FileInfo(props.inFile);
 
 			/* check offset/length */
-			if (offset > fw.inFInfo.Length)
+			if (Offset > fw.inFInfo.Length)
 			{
 				Console.Error.WriteLine(
 					Lang.Resource.Main_Warning_Prefix +
 					Lang.Tools.XorImageRes.Warning_LargeOffset);
-				offset = 0;
+				Offset = 0;
 			}
 
+			if (Length == long.MinValue)
+				Length = fw.inFInfo.Length - Offset;
 			/*
-			 * - len_sに何かが指定されている
-			 * - 文字列->long変換に失敗したか
 			 * - lenが0以下であるか
 			 * - lenが入力ファイルの長さからオフセットを引いたものより大きいか
 			 */
-			if (len_s != null &&
-			    (!Utils.StrToLong(len_s, out len, NumberStyles.None) ||
-			    len <= 0 ||
-			    len > fw.inFInfo.Length - offset))
+			if (Length <= 0 ||
+			    Length > fw.inFInfo.Length - Offset)
 			{
 				Console.Error.WriteLine(
 					Lang.Resource.Main_Warning_Prefix +
 					Lang.Tools.XorImageRes.Warning_InvalidLength);
-				len = long.MaxValue;
+				Length = fw.inFInfo.Length - Offset;
 			}
 			/* check offset/length end */
 
 			if (!props.quiet)
-				PrintInfo(len != long.MaxValue ? len : fw.inFInfo.Length - offset, in ptnAry);
+				PrintInfo(Length, in ptnAry);
 
 			try
 			{
@@ -255,40 +228,25 @@ namespace firmware_wintools.Tools
 				using (fw.outFs = new FileStream(props.outFile, FileMode.Create,
 							FileAccess.Write, FileShare.None))
 				{
-					if (rewrite && offset > 0)
+					if (Rewrite && Offset > 0)
 					{
 						fw.inFs.CopyTo(fw.outFs);
 						/* outFs.Lengthをoffsetまで切り詰める */
-						fw.outFs.SetLength(offset);
+						fw.outFs.SetLength(Offset);
 					}
 
 					/* inFs.Positionをoffsetに移動 */
-					fw.inFs.Seek(offset, SeekOrigin.Begin);
+					fw.inFs.Seek(Offset, SeekOrigin.Begin);
 
 					while ((read_len = fw.inFs.Read(fw.data, 0, fw.data.Length)) > 0)
 					{
-						write_len = read_len;
-
-						if (len != long.MaxValue)
-							/* 読み取った長さよりも残りの対象データ長が長い場合 */
-							if (len > read_len)
-								len -= read_len;
-							/* 残りデータ長が読み取った長さ以下である場合 */
-							else
-								write_len = (int)len;
+						write_len = (Length < read_len) ? (int)Length : read_len;
 
 						p_off = XorData(ref fw.data, write_len, in ptnAry, p_len, p_off);
-
 						fw.outFs.Write(fw.data, 0, write_len);
 
-						/*
-						 * 読み取った長さが残り対象データ長以上である場合breakしてXorと書き込みを終了
-						 * len <= read_lenであるならばlengthが正しい数値で指定され（long.MaxValueでない）、
-						 * なおかつ最後のブロックであるので、inFsから残りをoutFsへコピーするため読み取った
-						 * データ長から実際に書き込んだデータ長を差し引いたサイズで現在位置からマイナス方向に
-						 * Seekする
-						 */
-						if (len <= read_len)
+						Length -= write_len;
+						if (Length <= 0)
 						{
 							fw.inFs.Seek(-(read_len - write_len), SeekOrigin.Current);
 							break;
@@ -296,7 +254,7 @@ namespace firmware_wintools.Tools
 					}
 
 					/* copy remaining data in inFs to outFs if rewrite mode */
-					if (rewrite)
+					if (Rewrite)
 						fw.inFs.CopyTo(fw.outFs);
 				}
 			}
