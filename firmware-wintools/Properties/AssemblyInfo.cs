47e66a5ddf3e79fa26fe4124936ba51fc3bd3974
INAGAKI Hiroshi <musashino.open@gmail.com>
2024-05-28 22:11:41 +0900

    バージョンを 0.7.2 に引き上げ

--- firmware-wintools/Properties/AssemblyInfo.cs
+++ firmware-wintools/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Firmware-WinTools")]
-[assembly: AssemblyCopyright("Copyright ©musashino205  2019-2021")]
+[assembly: AssemblyCopyright("Copyright ©musashino205  2019-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.7.3.0")]
+[assembly: AssemblyFileVersion("0.7.3.0")]
