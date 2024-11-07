7ce2873ed614d51eeb36f6795b92bfa09109905a
INAGAKI Hiroshi <musashino.open@gmail.com>
2024-11-07 20:21:07 +0900

    7zip: 7-Zip LZMA SDKからLZMAライブラリを追加

---
+++ 7zip/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下を通して制御されます
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("lzma library from 7-Zip LZMA SDK")]
+[assembly: AssemblyDescription("'CS' source files picked from 7-Zip LZMA SDK.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("7-Zip.org")]
+[assembly: AssemblyProduct("7-Zip LZMA SDK")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("d6a3084f-4935-48d3-9e22-084164084d3d")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+[assembly: AssemblyVersion("24.08.0.0")]
+[assembly: AssemblyFileVersion("24.08.0.0")]
